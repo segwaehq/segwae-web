c2320bfac26bd5872241d0c5ee4105505e817fdd
henryoncode <ikennaunegbu10@gmail.com>
2025-10-19 03:11:01 +0100

    minor

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/lib/supabase'
+// import { createClient } from '@/lib/supabase'
+import { createClient } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
 export const dynamic = 'force-dynamic'
